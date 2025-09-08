3d5846e01cdc98b3d82aabb35a55521e0d01c7b5
vanesaarandavera <vanesaa715@gmail.com>
2025-09-08 18:38:31 -0300

    barra vendedor

--- VocesDePapelV1.1/Views/vendedor.Designer.cs
+++ VocesDePapelV1.1/Views/vendedor.Designer.cs
@@ -28,18 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            menuStrip1 = new MenuStrip();
+            ventaToolStripMenuItem = new ToolStripMenuItem();
+            librosToolStripMenuItem = new ToolStripMenuItem();
+            usuariosToolStripMenuItem = new ToolStripMenuItem();
+            reporteToolStripMenuItem = new ToolStripMenuItem();
+            menuStrip1.SuspendLayout();
             SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Anchor = AnchorStyles.Top;
+            menuStrip1.AutoSize = false;
+            menuStrip1.BackColor = Color.FromArgb(255, 192, 128);
+            menuStrip1.Dock = DockStyle.None;
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { ventaToolStripMenuItem, librosToolStripMenuItem, usuariosToolStripMenuItem, reporteToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.RightToLeft = RightToLeft.No;
+            menuStrip1.Size = new Size(800, 74);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // ventaToolStripMenuItem
+            // 
+            ventaToolStripMenuItem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            ventaToolStripMenuItem.Image = Properties.Resources.verificar;
+            ventaToolStripMenuItem.Name = "ventaToolStripMenuItem";
+            ventaToolStripMenuItem.Size = new Size(99, 70);
+            ventaToolStripMenuItem.Text = "Venta";
+            // 
+            // librosToolStripMenuItem
+            // 
+            librosToolStripMenuItem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            librosToolStripMenuItem.Image = Properties.Resources.libro;
+            librosToolStripMenuItem.Name = "librosToolStripMenuItem";
+            librosToolStripMenuItem.Size = new Size(101, 70);
+            librosToolStripMenuItem.Text = "Libros";
+            // 
+            // usuariosToolStripMenuItem
+            // 
+            usuariosToolStripMenuItem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            usuariosToolStripMenuItem.Image = Properties.Resources.usuario;
+            usuariosToolStripMenuItem.Name = "usuariosToolStripMenuItem";
+            usuariosToolStripMenuItem.RightToLeft = RightToLeft.No;
+            usuariosToolStripMenuItem.Size = new Size(124, 70);
+            usuariosToolStripMenuItem.Text = "Usuarios";
+            // 
+            // reporteToolStripMenuItem
+            // 
+            reporteToolStripMenuItem.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
+            reporteToolStripMenuItem.Image = Properties.Resources.aumentar;
+            reporteToolStripMenuItem.Name = "reporteToolStripMenuItem";
+            reporteToolStripMenuItem.Size = new Size(115, 70);
+            reporteToolStripMenuItem.Text = "Reporte";
             // 
             // vendedor
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
             Name = "vendedor";
             Text = "Vendedor";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem ventaToolStripMenuItem;
+        private ToolStripMenuItem librosToolStripMenuItem;
+        private ToolStripMenuItem usuariosToolStripMenuItem;
+        private ToolStripMenuItem reporteToolStripMenuItem;
     }
 }
