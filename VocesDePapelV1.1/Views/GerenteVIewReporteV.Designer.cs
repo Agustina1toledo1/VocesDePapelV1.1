52eb41352931672a1c1e64cb8f4919a48b14bcf2
AgusT <agustina1toledo1@gmail.com>
2025-09-09 12:30:19 -0300

    modificacion de usuario view gerente

--- VocesDePapelV1.1/Views/GerenteVIewReporteV.Designer.cs
+++ VocesDePapelV1.1/Views/GerenteVIewReporteV.Designer.cs
@@ -28,12 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "GerenteVIewReporteV";
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(264, 215);
+            label1.Name = "label1";
+            label1.Size = new Size(311, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Hola, soy el Formulario Gerente reporte venta";
+            // 
+            // GerenteVIewReporteV
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "GerenteVIewReporteV";
+            Text = "GerenteVIewReporteV";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
     }
 }
