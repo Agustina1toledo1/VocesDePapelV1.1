7c3d62135a93395dcd84b0d205192dff982eb16e
AgusT <agustina1toledo1@gmail.com>
2025-09-09 17:19:14 -0300

    Se agrego los campos para agregar, modificar y eliminar usuario

--- VocesDePapelV1.1/Views/GerenteViewUsuario.cs
+++ VocesDePapelV1.1/Views/GerenteViewUsuario.cs
@@ -16,5 +16,10 @@ namespace VocesDePapelV1._1.Views
         {
             InitializeComponent();
         }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
