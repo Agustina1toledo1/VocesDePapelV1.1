57d1a9f15c07a2de8f7dfffeb4ee2f4821169905
AgusT <agustina1toledo1@gmail.com>
2025-09-11 15:48:20 -0300

    Se crearon las vistas para Reporte de Venta y producto para admin

---
+++ VocesDePapelV1.1/Views/AdministradorViewReporteVenta.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace VocesDePapelV1._1.Views
+{
+    partial class AdministradorViewReporteVenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(408, 175);
+            label1.Name = "label1";
+            label1.Size = new Size(323, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Hola, soy el form de reporte de venta en admin";
+            // 
+            // AdministradorViewReporteVenta
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label1);
+            Name = "AdministradorViewReporteVenta";
+            Text = "AdministradorViewReporteVenta";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+    }
+}
