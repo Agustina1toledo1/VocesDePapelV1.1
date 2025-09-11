5eb9d0bc8a9757d5089513ce418cd0fe0830072c
AgusT <agustina1toledo1@gmail.com>
2025-09-11 14:56:20 -0300

    Se modifico la visata del CRUD de categoria para admin

--- VocesDePapelV1.1/Views/AdministradorViewCategorias.Designer.cs
+++ VocesDePapelV1.1/Views/AdministradorViewCategorias.Designer.cs
@@ -28,32 +28,255 @@
         /// </summary>
         private void InitializeComponent()
         {
+            DataGridViewCellStyle dataGridViewCellStyle3 = new DataGridViewCellStyle();
+            DataGridViewCellStyle dataGridViewCellStyle4 = new DataGridViewCellStyle();
+            l_detalle_categoria = new Label();
+            btn_eliminar_autor = new Button();
+            btn_modificar_autor = new Button();
+            btn_registrar_autor = new Button();
+            cmb_estado_categoria = new ComboBox();
+            text_nombre_categoria_admin = new TextBox();
+            l_estado_categoria = new Label();
+            l_nombre_categoria = new Label();
             label1 = new Label();
+            btn_buscar_categoria = new Button();
+            text_buscar_categoria = new TextBox();
+            l_buscar_categoria = new Label();
+            l_lista_categorias = new Label();
+            dataGridView1 = new DataGridView();
+            btn_seleccionar = new DataGridViewButtonColumn();
+            nombre = new DataGridViewTextBoxColumn();
+            estado = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
+            // 
+            // l_detalle_categoria
+            // 
+            l_detalle_categoria.AutoSize = true;
+            l_detalle_categoria.BackColor = Color.White;
+            l_detalle_categoria.Font = new Font("Segoe UI", 13.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            l_detalle_categoria.Location = new Point(62, 40);
+            l_detalle_categoria.Name = "l_detalle_categoria";
+            l_detalle_categoria.Size = new Size(256, 31);
+            l_detalle_categoria.TabIndex = 63;
+            l_detalle_categoria.Text = "Detalle de la Categoria";
+            // 
+            // btn_eliminar_autor
+            // 
+            btn_eliminar_autor.BackColor = Color.FromArgb(255, 128, 128);
+            btn_eliminar_autor.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_eliminar_autor.Location = new Point(61, 372);
+            btn_eliminar_autor.Name = "btn_eliminar_autor";
+            btn_eliminar_autor.Size = new Size(269, 43);
+            btn_eliminar_autor.TabIndex = 62;
+            btn_eliminar_autor.Text = "Eliminar";
+            btn_eliminar_autor.UseVisualStyleBackColor = false;
+            // 
+            // btn_modificar_autor
+            // 
+            btn_modificar_autor.BackColor = Color.FromArgb(255, 192, 128);
+            btn_modificar_autor.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_modificar_autor.Location = new Point(61, 310);
+            btn_modificar_autor.Name = "btn_modificar_autor";
+            btn_modificar_autor.Size = new Size(269, 43);
+            btn_modificar_autor.TabIndex = 61;
+            btn_modificar_autor.Text = "Modificar";
+            btn_modificar_autor.UseVisualStyleBackColor = false;
+            // 
+            // btn_registrar_autor
+            // 
+            btn_registrar_autor.BackColor = SystemColors.GradientActiveCaption;
+            btn_registrar_autor.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_registrar_autor.Location = new Point(62, 250);
+            btn_registrar_autor.Name = "btn_registrar_autor";
+            btn_registrar_autor.Size = new Size(269, 43);
+            btn_registrar_autor.TabIndex = 60;
+            btn_registrar_autor.Text = "Registrar";
+            btn_registrar_autor.UseVisualStyleBackColor = false;
+            // 
+            // cmb_estado_categoria
+            // 
+            cmb_estado_categoria.DropDownStyle = ComboBoxStyle.DropDownList;
+            cmb_estado_categoria.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            cmb_estado_categoria.FormattingEnabled = true;
+            cmb_estado_categoria.Location = new Point(61, 193);
+            cmb_estado_categoria.Name = "cmb_estado_categoria";
+            cmb_estado_categoria.Size = new Size(269, 36);
+            cmb_estado_categoria.TabIndex = 59;
+            // 
+            // text_nombre_categoria_admin
+            // 
+            text_nombre_categoria_admin.Font = new Font("Segoe UI", 12F);
+            text_nombre_categoria_admin.Location = new Point(61, 124);
+            text_nombre_categoria_admin.Name = "text_nombre_categoria_admin";
+            text_nombre_categoria_admin.Size = new Size(269, 34);
+            text_nombre_categoria_admin.TabIndex = 58;
+            // 
+            // l_estado_categoria
+            // 
+            l_estado_categoria.AutoSize = true;
+            l_estado_categoria.BackColor = Color.White;
+            l_estado_categoria.Font = new Font("Segoe UI", 12F);
+            l_estado_categoria.Location = new Point(64, 161);
+            l_estado_categoria.Name = "l_estado_categoria";
+            l_estado_categoria.Size = new Size(71, 28);
+            l_estado_categoria.TabIndex = 57;
+            l_estado_categoria.Text = "Estado";
+            // 
+            // l_nombre_categoria
+            // 
+            l_nombre_categoria.AutoSize = true;
+            l_nombre_categoria.BackColor = Color.White;
+            l_nombre_categoria.Font = new Font("Segoe UI", 12F);
+            l_nombre_categoria.Location = new Point(65, 93);
+            l_nombre_categoria.Name = "l_nombre_categoria";
+            l_nombre_categoria.Size = new Size(85, 28);
+            l_nombre_categoria.TabIndex = 56;
+            l_nombre_categoria.Text = "Nombre";
             // 
             // label1
             // 
-            label1.AutoSize = true;
-            label1.Location = new Point(304, 215);
+            label1.BackColor = Color.White;
+            label1.BorderStyle = BorderStyle.FixedSingle;
+            label1.Dock = DockStyle.Left;
+            label1.Location = new Point(0, 0);
             label1.Name = "label1";
-            label1.Size = new Size(213, 20);
-            label1.TabIndex = 1;
-            label1.Text = "Hola soy el form de categorias";
+            label1.Size = new Size(399, 464);
+            label1.TabIndex = 55;
+            // 
+            // btn_buscar_categoria
+            // 
+            btn_buscar_categoria.BackColor = SystemColors.GradientActiveCaption;
+            btn_buscar_categoria.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btn_buscar_categoria.Location = new Point(1249, 35);
+            btn_buscar_categoria.Name = "btn_buscar_categoria";
+            btn_buscar_categoria.Size = new Size(66, 43);
+            btn_buscar_categoria.TabIndex = 68;
+            btn_buscar_categoria.UseVisualStyleBackColor = false;
+            // 
+            // text_buscar_categoria
+            // 
+            text_buscar_categoria.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            text_buscar_categoria.Location = new Point(887, 40);
+            text_buscar_categoria.Name = "text_buscar_categoria";
+            text_buscar_categoria.Size = new Size(329, 31);
+            text_buscar_categoria.TabIndex = 67;
+            // 
+            // l_buscar_categoria
+            // 
+            l_buscar_categoria.AutoSize = true;
+            l_buscar_categoria.BackColor = Color.White;
+            l_buscar_categoria.Font = new Font("Segoe UI", 10.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            l_buscar_categoria.Location = new Point(800, 43);
+            l_buscar_categoria.Name = "l_buscar_categoria";
+            l_buscar_categoria.Size = new Size(63, 25);
+            l_buscar_categoria.TabIndex = 66;
+            l_buscar_categoria.Text = "Buscar";
+            // 
+            // l_lista_categorias
+            // 
+            l_lista_categorias.BackColor = Color.White;
+            l_lista_categorias.Font = new Font("Segoe UI", 16.2F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            l_lista_categorias.Location = new Point(414, 20);
+            l_lista_categorias.Name = "l_lista_categorias";
+            l_lista_categorias.Size = new Size(903, 66);
+            l_lista_categorias.TabIndex = 65;
+            l_lista_categorias.Text = "Lista de categorias";
+            l_lista_categorias.TextAlign = ContentAlignment.MiddleLeft;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = SystemColors.Control;
+            dataGridViewCellStyle3.Font = new Font("Segoe UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            dataGridViewCellStyle3.ForeColor = SystemColors.WindowText;
+            dataGridViewCellStyle3.Padding = new Padding(2);
+            dataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = DataGridViewTriState.True;
+            dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { btn_seleccionar, nombre, estado });
+            dataGridView1.Location = new Point(414, 116);
+            dataGridView1.MultiSelect = false;
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridViewCellStyle4.SelectionBackColor = Color.White;
+            dataGridViewCellStyle4.SelectionForeColor = Color.Black;
+            dataGridView1.RowsDefaultCellStyle = dataGridViewCellStyle4;
+            dataGridView1.Size = new Size(903, 304);
+            dataGridView1.TabIndex = 64;
+            // 
+            // btn_seleccionar
+            // 
+            btn_seleccionar.HeaderText = "";
+            btn_seleccionar.MinimumWidth = 6;
+            btn_seleccionar.Name = "btn_seleccionar";
+            btn_seleccionar.ReadOnly = true;
+            btn_seleccionar.Width = 30;
+            // 
+            // nombre
+            // 
+            nombre.HeaderText = "Nombre";
+            nombre.MinimumWidth = 6;
+            nombre.Name = "nombre";
+            nombre.ReadOnly = true;
+            nombre.Width = 125;
+            // 
+            // estado
+            // 
+            estado.HeaderText = "Estado";
+            estado.MinimumWidth = 6;
+            estado.Name = "estado";
+            estado.ReadOnly = true;
+            estado.Width = 125;
             // 
             // AdministradorViewCategorias
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            ClientSize = new Size(1340, 464);
+            Controls.Add(btn_buscar_categoria);
+            Controls.Add(text_buscar_categoria);
+            Controls.Add(l_buscar_categoria);
+            Controls.Add(l_lista_categorias);
+            Controls.Add(dataGridView1);
+            Controls.Add(l_detalle_categoria);
+            Controls.Add(btn_eliminar_autor);
+            Controls.Add(btn_modificar_autor);
+            Controls.Add(btn_registrar_autor);
+            Controls.Add(cmb_estado_categoria);
+            Controls.Add(text_nombre_categoria_admin);
+            Controls.Add(l_estado_categoria);
+            Controls.Add(l_nombre_categoria);
             Controls.Add(label1);
             Name = "AdministradorViewCategorias";
             Text = "AdministradorViewCategorias";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
 
         #endregion
 
+        private Label l_detalle_categoria;
+        private Button btn_eliminar_autor;
+        private Button btn_modificar_autor;
+        private Button btn_registrar_autor;
+        private ComboBox cmb_estado_categoria;
+        private TextBox text_nombre_categoria_admin;
+        private Label l_estado_categoria;
+        private Label l_nombre_categoria;
         private Label label1;
+        private Button btn_buscar_categoria;
+        private TextBox text_buscar_categoria;
+        private Label l_buscar_categoria;
+        private Label l_lista_categorias;
+        private DataGridView dataGridView1;
+        private DataGridViewButtonColumn btn_seleccionar;
+        private DataGridViewTextBoxColumn nombre;
+        private DataGridViewTextBoxColumn estado;
     }
 }
