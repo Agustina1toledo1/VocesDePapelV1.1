9f3cf4f2f0ccf802ed62eeae325e45b9f76c1356
AgusT <agustina1toledo1@gmail.com>
2025-09-13 19:22:47 -0300

    Se creo la vista para el login.

--- VocesDePapelV1.1/Properties/Resources.Designer.cs
+++ VocesDePapelV1.1/Properties/Resources.Designer.cs
@@ -93,6 +93,16 @@ namespace VocesDePapelV1._1.Properties {
         /// <summary>
         ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap logo {
+            get {
+                object obj = ResourceManager.GetObject("logo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap lupa {
             get {
                 object obj = ResourceManager.GetObject("lupa", resourceCulture);
