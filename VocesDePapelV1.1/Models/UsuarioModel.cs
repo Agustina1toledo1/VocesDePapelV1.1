fa629de69d29db5efb399b567384884381993f19
AgusT <agustina1toledo1@gmail.com>
2025-09-14 19:00:15 -0300

    Se creo el modelo de usuario

---
+++ VocesDePapelV1.1/Models/UsuarioModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocesDePapelV1._1.Models
+{
+    public class UsuarioModel
+    {
+        //campos de la tabla Usuario
+        private int id_usuario;
+        private string nombre;
+        private string apellido;
+        private string clave;
+        private long cuit_usuario;
+        private bool baja;
+        private int id_rol;
+
+    }
+}
