37afbc52dad636f20ee419c952abdc0181ed87a4
AgusT <agustina1toledo1@gmail.com>
2025-09-14 20:07:15 -0300

    Se creo la interfa para el comportamiento de usuario.

---
+++ VocesDePapelV1.1/Models/IUsuarioRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//define comportamiento
+namespace VocesDePapelV1._1.Models
+{
+    public interface IUsuarioRepository
+    {
+        void Add(UsuarioModel usuario);
+        void Modificar(UsuarioModel usuario);
+        void Eliminar(UsuarioModel usuario);
+        IEnumerable<UsuarioModel> GetAll(); //listar todos
+        IEnumerable<UsuarioModel> GetByValue(); //buscar por valor
+    }
+}
--- VocesDePapelV1.1/Models/UsuarioModel.cs
+++ VocesDePapelV1.1/Models/UsuarioModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace VocesDePapelV1._1.Models
 {
@@ -18,5 +20,58 @@ namespace VocesDePapelV1._1.Models
         private bool baja;
         private int id_rol;
 
+        //accesors y mutators. Validaciones
+        [DisplayName ("Usuario ID")]
+        public int Id_usuario 
+        { 
+            get { return id_usuario; }
+            set { id_usuario = value; } 
+        }
+        [DisplayName("Nombre del usuario")]
+        [Required(ErrorMessage = "El nombre de usuario es requerido")]
+        [RegularExpression(@"^[a-zA-Z]{3,50}$", ErrorMessage = "Solo se permiten letras y  debe tener entre 3 y 50 caracteres.")]
+        public string Nombre 
+        { 
+            get { return nombre; }
+            set { nombre = value; }            
+        }
+        [DisplayName("Apellido del usuario")]
+        [Required(ErrorMessage = "El apellido de usuario es requerido")]
+        [RegularExpression(@"^[a-zA-Z]{3,50}$", ErrorMessage = "Solo se permiten letras y  debe tener entre 3 y 50 caracteres.")]
+        public string Apellido 
+        { 
+            get { return apellido; }            
+            set { apellido = value; }             
+        }
+        [DisplayName("Clave del usuario")]
+        [Required(ErrorMessage = "La clave del usuario es requerido")]
+        [StringLength( 8, ErrorMessage = "La contraseña debe contener al menos 8 caracteres")]
+        public string Clave 
+        { 
+            get { return clave; }  
+            set { clave = value; }
+        }
+        [DisplayName("Cuit del usuario")]
+        [Required(ErrorMessage = "El cuiy del usuario es requerido")]
+        [StringLength(11, ErrorMessage = "El cuit no puede ser mayor a 11 caracteres")]
+        [RegularExpression("(^[0-9]){11}$", ErrorMessage = "Solo se permiten números y debe contener 11 caracteres\"")] //^ indica inicio de la cadena, $ indica fin de la cadena, [0-9] indica que solo se permiten números, + indica que se permiten uno o más caracteres
+        public long Cuit_usuario { 
+            get { return cuit_usuario; }             
+            set { cuit_usuario = value; }             
+        }
+        [DisplayName("Estado del usuario")]
+        [Required(ErrorMessage = "El estado del usuario es requerido")]
+        public bool Baja 
+        { 
+            get { return baja; }
+            set { baja = value; }
+        }
+        [DisplayName("Rol del usuario")]
+        [Required(ErrorMessage = "El rol del usuario es requerido")]
+        public int Id_rol 
+        { 
+            get { return id_rol; }            
+            set { id_rol = value; }            
+        }
     }
 }
