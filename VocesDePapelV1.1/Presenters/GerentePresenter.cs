c358b93c1e1e3dcbb1d4b1596c359c5e94e5dc68
AgusT <agustina1toledo1@gmail.com>
2025-09-15 13:56:37 -0300

    Se modifico el presentador de gerente: se creo el metodo ShowUsuariosView

--- VocesDePapelV1.1/Presenters/GerentePresenter.cs
+++ VocesDePapelV1.1/Presenters/GerentePresenter.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using VocesDePapelV1._1.Views;
+using VocesDePapelV1._1.Models;
+using VocesDePapelV1._1.Repositories;
 
 namespace VocesDePapelV1._1.Presenters
 {
-    internal class GerentePresenter
+    public class GerentePresenter
     {
+        private IGerenteView view;
+        //un campo de solo lectura para la cadena de conexion
+        private readonly string connectionString;
+
+        public GerentePresenter(IGerenteView view, string connectionString)
+        {
+            this.view = view;
+            this.connectionString = connectionString;
+
+            //suscribirse a los eventos de la vista
+            this.view.ShowUsuarioView += ShowUsuariosView;
+
+        }
+        //mostrar vista de usuario
+        private void ShowUsuariosView(object? sender, EventArgs e)
+        {
+            IGerenteViewUsuario view = new GerenteViewUsuario();
+            IUsuarioRepository repository = new UsuarioRepository(connectionString);
+            new UsuarioPresenter(view, repository);
+        }
     }
 }
