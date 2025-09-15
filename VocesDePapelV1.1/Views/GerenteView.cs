ed314e55aae2f468f70d7b91ab8264bc1bc8f00c
AgusT <agustina1toledo1@gmail.com>
2025-09-15 13:41:18 -0300

    Se elimino la interfaz gerente VIew porque era una clase

--- VocesDePapelV1.1/Views/GerenteView.cs
+++ VocesDePapelV1.1/Views/GerenteView.cs
@@ -9,12 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace VocesDePapelV1._1.Views
-{
-    public partial class GerenteView : Form
+
+    public partial class GerenteView : Form, IGerenteView // <-- ERROR: IGerenteView es una clase, no una interfaz
     {
         public GerenteView()
         {
             InitializeComponent();
         }
+    }
+    // Cambia la clase a interfaz
+    public interface IGerenteView
+    {
+        event EventHandler ShowUsuarioView;
+        event EventHandler ShowBackupView;
+        event EventHandler ShowReporteVentaView;
+        event EventHandler ShowReporteLibroView;
     }
 }
