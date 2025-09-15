ad8c7bf1f058d3a9bfa849b85f938f1d6d36cf12
AgusT <agustina1toledo1@gmail.com>
2025-09-14 21:11:02 -0300

    Se creo la interfaz de la vista de usuario

---
+++ VocesDePapelV1.1/Views/IGerenteViewUsuario.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocesDePapelV1._1.Views
+{
+    public interface IGerenteViewUsuario
+    {
+        //propiedades
+        string UsuarioId{ get; set; }
+        string UsuarioNombre { get; set; }
+        string UsuarioApellido { get; set; }
+        string Clave { get; set; }
+        string CuitUsuario { get; set; }
+        bool Baja { get; set; }
+        string UsuarioIdRol { get; set; }
+
+        //Eventos
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        //Metodos
+        //enlace a la lista de usuarios
+        void SetUsuarioListBindingSource(BindingSource usuarioList);
+        void Show(); //mostrar formulario
+    }
+}
