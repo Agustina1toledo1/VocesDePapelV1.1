dd71aa566bc1ac51a651ff74dad17dd1fcb62137
AgusT <agustina1toledo1@gmail.com>
2025-09-17 09:12:06 -0300

    Se restauro la conexion a la base de datos

--- VocesDePapelV1.1/Program.cs
+++ VocesDePapelV1.1/Program.cs
@@ -3,6 +3,7 @@ using VocesDePapelV1._1.Views;
 using VocesDePapelV1._1.Repositories;
 using VocesDePapelV1._1.Models;
 using System.Configuration;
+using System;
 namespace VocesDePapelV1._1
 {
     internal static class Program
@@ -16,12 +17,15 @@ namespace VocesDePapelV1._1
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-
+            string connectionString = ConfigurationManager.ConnectionStrings["SqlConnection"].ConnectionString;
             //string connectionString = ConfigurationManager.ConnectionStrings["SqlConnection2"].ConnectionString;
-            //IGerenteView view = new Views.GerenteView();
+            //probamos el presenter de usuario
+            IGerenteViewUsuario view = new GerenteViewUsuario();
+            IUsuarioRepository repository = new UsuarioRepository(connectionString);
+            new UsuarioPresenter(view, repository);
             //new GerentePresenter(view, connectionString);
             //Application.Run(new Views.GerenteView());
-            //Application.Run((Form)view);
+            Application.Run((Form)view);
         }
     }
 }
