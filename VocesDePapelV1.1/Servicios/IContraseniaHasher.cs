5b4203d2fc0e52d592971c5d1c30c1b2d4cb6b39
AgusT <agustina1toledo1@gmail.com>
2025-09-21 13:44:54 -0300

    Se creo la interfaz IContraseniaHasher dentro de la carpeta servicios

---
+++ VocesDePapelV1.1/Servicios/IContraseniaHasher.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocesDePapelV1._1.Servicios
+{
+    internal interface IContraseniaHasher
+    {
+        string Hash(string contraseniaPlana);
+        bool Verify(string contraseniaPlana, string almacenaHash);
+    }
+}
