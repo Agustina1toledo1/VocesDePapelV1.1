f7589070914d8176f9dc98889315811725d9bc64
AgusT <agustina1toledo1@gmail.com>
2025-10-04 17:40:17 -0300

    Se creo la interfaz del repositorio del categoria

--- VocesDePapelV1.1/Presenters/CategoriaAmdinPresenter.cs
+++ VocesDePapelV1.1/Presenters/CategoriaAmdinPresenter.cs
@@ -4,12 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using VocesDePapelV1._1.Views;
+using VocesDePapelV1._1.Models;
+using VocesDePapelV1._1.Presenters.Common;
+using VocesDePapelV1._1.Servicios;
 
 namespace VocesDePapelV1._1.Presenters
 {
     public class CategoriaAmdinPresenter
     {
+        //campos     
         private IAdministradorCategoria view; //campo privado para la vista usando la interfaz
+        private ICategoriaRepository repository; //campo privado para el repositorio usando la interfaz
+        private IContraseniaHasher hasher; //campo privado para el hasher usando la interfaz
+        private BindingSource usuarioBindingSource; //origen de datos para el enlace
+        private BindingSource estadoBindingSource; //origen de datos para el enlace
+        private BindingSource rolBindingSource; //origen de datos para el enlace
+        private IEnumerable<UsuarioModel> usuarioList; //lista de usuarios
+        private IEnumerable<RolModel> rolList;
+        private IEnumerable<EstadoModel> estadoList;
         public CategoriaAmdinPresenter(IAdministradorCategoria view)
         {
             this.view = view;
