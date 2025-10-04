923b880b445ffc03f80dd1bc169e103e38f594e9
AgusT <agustina1toledo1@gmail.com>
2025-10-04 17:36:06 -0300

    Se modifico la interfaz de categoria del admin

--- VocesDePapelV1.1/Views/AdministradorViewCategorias.cs
+++ VocesDePapelV1.1/Views/AdministradorViewCategorias.cs
@@ -12,12 +12,37 @@ namespace VocesDePapelV1._1.Views
 {
     public partial class AdministradorViewCategorias : Form, IAdministradorCategoria
     {
+        private string message;
+        private bool isSuccessful;
         public AdministradorViewCategorias()
         {
             InitializeComponent();
+            AssociateAndRaiseViewEvents(); //asociar y generar los eventos de vistas
         }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            throw new NotImplementedException();
+        }
+
         //singleton patron (abre una sola instancia del formulario) 
         private static AdministradorViewCategorias instance;
+
+        public event EventHandler SearchEvent;
+        public event EventHandler AddNewEvent;
+        public event EventHandler EditEvent;
+        public event EventHandler DeleteEvent;
+        public event EventHandler SaveEvent;
+        public event EventHandler CancelEvent;
+
+        public string CategoriaId { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string NombreCategoria { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Estado { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string NombreEstado { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string SearchValue { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public bool IsSuccessful { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string Message { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
         public static AdministradorViewCategorias GetInstance(Form parentConteiner)
         {
             if (instance == null || instance.IsDisposed) //si es nulo o esta desechado
@@ -37,6 +62,16 @@ namespace VocesDePapelV1._1.Views
 
             }
             return instance;
+        }
+
+        public void SetCategoriaListBindingSource(BindingSource usuarioList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetEstadoListBindingSource(BindingSource estadoList)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- VocesDePapelV1.1/Views/IAdministradorCategoria.cs
+++ VocesDePapelV1.1/Views/IAdministradorCategoria.cs
@@ -8,6 +8,28 @@ namespace VocesDePapelV1._1.Views
 {
     public interface IAdministradorCategoria
     {
+        //propiedades
+        string CategoriaId { get; set; }
+        string NombreCategoria { get; set; }
+        string Estado { get; set; }
+        string NombreEstado { get; set; }
+        string SearchValue { get; set; }
+        //bool IsEdit { get; set; }
+        bool IsSuccessful { get; set; }
+        string Message { get; set; }
+
+        //Eventos
+        event EventHandler SearchEvent;
+        event EventHandler AddNewEvent;
+        event EventHandler EditEvent;
+        event EventHandler DeleteEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CancelEvent;
+
+        //Metodos
+        //enlace a la lista de categorias
+        void SetCategoriaListBindingSource(BindingSource usuarioList);
+        void SetEstadoListBindingSource(BindingSource estadoList);
         void Show(); //metodo para mostrar la vista
     }
 }
