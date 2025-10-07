4594100ac45d3c10990eecefc441dba84cd9a11f
vanesaarandavera <vanesaa715@gmail.com>
2025-10-07 19:13:13 -0300

    se creo el repositorio de clientes

---
+++ VocesDePapelV1.1/Repositories/IClienteRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VocesDePapelV1._1.Models;
+
+namespace VocesDePapelV1._1.Repositories
+{
+    public interface IClienteRepository
+    {
+        void Add(ClienteModel cliente);
+        void Modificar(ClienteModel cliente);
+        void Eliminar(int id);
+        IEnumerable<ClienteModel> GetAll();
+        IEnumerable<ClienteModel> GetByValue(string value);
+    }
+}
