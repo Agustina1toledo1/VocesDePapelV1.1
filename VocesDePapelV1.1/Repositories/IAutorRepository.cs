018fb566c3f0beed3a167a3086a386c7da24c5b9
AgusT <agustina1toledo1@gmail.com>
2025-10-08 20:36:37 -0300

    Se creo el repositorio del autor

---
+++ VocesDePapelV1.1/Repositories/IAutorRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VocesDePapelV1._1.Models;
+
+namespace VocesDePapelV1._1.Repositories
+{
+    public interface IAutorRepository
+    {
+        void Add(AutorModel autor);
+        void Modificar(AutorModel autor);
+        void Eliminar(int id);
+        IEnumerable<AutorModel> GetAll(); //listar todos
+        IEnumerable<AutorModel> GetByValue(string value); //buscar por valor
+        IEnumerable<EstadoModel> GetEstado();
+    }
+}
