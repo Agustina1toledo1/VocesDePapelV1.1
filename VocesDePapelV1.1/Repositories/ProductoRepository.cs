76075eb8af9be7f5b54f16a1078d9629cb673375
AgusT <agustina1toledo1@gmail.com>
2025-10-11 21:00:40 -0300

    Se modifico el presentador de producto

--- VocesDePapelV1.1/Repositories/ProductoRepository.cs
+++ VocesDePapelV1.1/Repositories/ProductoRepository.cs
@@ -139,17 +139,98 @@ namespace VocesDePapelV1._1.Repositories
 
         public IEnumerable<CategoriaModel> GetCategoria()
         {
-            throw new NotImplementedException();
+            var categoriaList = new List<CategoriaModel>();
+
+            using (var connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
+            using (var command = new Microsoft.Data.SqlClient.SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = @"SELECT id_categoria, nombre_categoria
+                                        FROM categoria 
+                                        ORDER BY id_categoria DESC";
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var categoria = new CategoriaModel
+                        {
+                            Id_categoria = Convert.ToInt32(reader["id_categoria"]),
+                            Nombre_categoria = reader["nombre_categoria"].ToString(),
+                            Estado_id = Convert.ToInt32(reader["id_estado"])
+                        };
+                        categoriaList.Add(categoria);
+                    }
+                }
+            }
+            return categoriaList;
         }
 
         public IEnumerable<EstadoModel> GetEstado()
         {
-            throw new NotImplementedException();
+            //lista de estados
+            var estadoList = new List<EstadoModel>();
+            //consultas sql
+            using (var connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
+            using (var command = new Microsoft.Data.SqlClient.SqlCommand())
+            {
+                connection.Open();
+                command.Connection = connection;
+                command.CommandText = "SELECT *  FROM estado ORDER BY id_estado DESC";
+                using (var reader = command.ExecuteReader())
+                {
+
+                    while (reader.Read())
+                    {
+                        var estado = new EstadoModel
+                        {
+                            Id_estado = Convert.ToInt32(reader["id_estado"]),
+                            Nombre_estado = reader["nombre_estado"].ToString(),
+                        };
+                        estadoList.Add(estado);
+                    }
+
+                }
+            }
+            return estadoList;
         }
 
         public void Modificar(ProductoModel producto)
         {
-            throw new NotImplementedException();
+            using (var connection = new Microsoft.Data.SqlClient.SqlConnection(connectionString))
+            using (var command = connection.CreateCommand())
+            {
+                connection.Open();
+                command.CommandText = @"
+                                    SELECT COUNT(1)
+                                        FROM producto
+                                        WHERE titulo = @titulo;
+                                        ";
+                command.Parameters.Add("@titulo", SqlDbType.NVarChar).Value = producto.Titulo;
+
+                int existe = (int)command.ExecuteScalar();
+                if (existe > 0)
+                {
+                    // Retiro los parámetros previos y lanzo excepción
+                    command.Parameters.Clear();
+                    throw new InvalidOperationException("El producto ingresado ya está registrado.");
+                }
+                // Inserto el nuevo producto
+                command.Parameters.Clear();
+                command.CommandText = @"UPDATE producto SET titulo =@titulo,
+                                        editorial =@editorial,
+                                        precio=@precio
+                                        stock=@stock,
+                                        eliminado =@estado,
+                                        id_categoria =@categoria";
+                command.Parameters.Add("@titulo", SqlDbType.NVarChar).Value = producto.Titulo;
+                command.Parameters.Add("@editorial", SqlDbType.NVarChar).Value = producto.Editorial;
+                command.Parameters.Add("@stock", SqlDbType.Int).Value = producto.Stock;
+                command.Parameters.Add("@precio", SqlDbType.Decimal).Value = producto.Precio;
+                command.Parameters.Add("@estado", SqlDbType.Int).Value = producto.Eliminado_id;
+                command.Parameters.Add("@categoria", SqlDbType.Int).Value = producto.Id_categoria;
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
