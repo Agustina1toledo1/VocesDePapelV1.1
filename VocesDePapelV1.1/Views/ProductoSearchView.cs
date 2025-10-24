4a69fda0f94a34b36de084091538e6381bbff393
vanesaarandavera <vanesaa715@gmail.com>
2025-10-24 18:35:58 -0300

    se agrego busqueda de libros en la vebta

--- VocesDePapelV1.1/Views/ProductoSearchView.cs
+++ VocesDePapelV1.1/Views/ProductoSearchView.cs
@@ -11,17 +11,22 @@ using VocesDePapelV1._1.Models;
 
 namespace VocesDePapelV1._1.Views
 {
-    public partial class ProductoSearchView : Form
-    {
-        public ProductoModel ProductoSeleccionado { get; private set; }
-        private IEnumerable<ProductoModel> productos;
+    public partial class ProductoSearchView : Form, IProductoSearchView
 
+    {
+        
+        private IEnumerable<ProductoModel> productos;
+        public event EventHandler<ProductoSeleccionadoEventArgs> ProductoSeleccionadoEvent;
+
+        public ProductoModel ProductoSeleccionado { get; private set; }
         public ProductoSearchView(IEnumerable<ProductoModel> productos)
         {
             InitializeComponent();
             this.productos = productos;
             CargarProductos();
+            AssociateAndRaiseViewEvents();
         }
+
 
         private void CargarProductos()
         {
@@ -38,52 +43,96 @@ namespace VocesDePapelV1._1.Views
             {
                 DataPropertyName = "Id_libro",
                 HeaderText = "ID",
-                Width = 50
+                Width = 50,
+                 ReadOnly = true
             });
 
             dataGridViewProductos.Columns.Add(new DataGridViewTextBoxColumn()
             {
                 DataPropertyName = "Titulo",
                 HeaderText = "Título",
-                Width = 200
+                Width = 200,
+                ReadOnly = true
             });
 
             dataGridViewProductos.Columns.Add(new DataGridViewTextBoxColumn()
             {
                 DataPropertyName = "Precio",
                 HeaderText = "Precio",
-                DefaultCellStyle = new DataGridViewCellStyle { Format = "C2" }
+                DefaultCellStyle = new DataGridViewCellStyle { Format = "C2" },
+                ReadOnly = true
             });
 
             dataGridViewProductos.Columns.Add(new DataGridViewTextBoxColumn()
             {
                 DataPropertyName = "Stock",
-                HeaderText = "Stock"
+                HeaderText = "Stock",
+                ReadOnly = true
+            });
+
+            dataGridViewProductos.Columns.Add(new DataGridViewTextBoxColumn()
+            {
+                DataPropertyName = "Nombre_categoria",
+                HeaderText = "Categoría",
+                ReadOnly = true
             });
         }
-
-        private void btnSeleccionar_Click(object sender, EventArgs e)
+        private void AssociateAndRaiseViewEvents()
         {
-            if (dataGridViewProductos.CurrentRow != null)
+            btnSeleccionar.Click += (s, e) => SeleccionarProducto();
+
+            btnCancelar.Click += (s, e) =>
             {
-                ProductoSeleccionado = (ProductoModel)dataGridViewProductos.CurrentRow.DataBoundItem;
+                ProductoSeleccionado = null;
+                this.DialogResult = DialogResult.Cancel;
+                this.Close();
+            };
+
+            dataGridViewProductos.CellDoubleClick += (s, e) =>
+                {
+                    if (e.RowIndex >= 0)
+                    {
+                        SeleccionarProducto();
+                    }
+                };
+            // Enter para seleccionar
+            dataGridViewProductos.KeyDown += (s, e) =>
+            {
+                if (e.KeyCode == Keys.Enter)
+                {
+                    SeleccionarProducto();
+                    e.Handled = true;
+                }
+            };
+        }
+        private void SeleccionarProducto()
+        {
+            if (dataGridViewProductos.CurrentRow != null && dataGridViewProductos.CurrentRow.DataBoundItem is ProductoModel producto)
+            {
+                ProductoSeleccionado = producto;
+
+                // Disparar evento
+                ProductoSeleccionadoEvent?.Invoke(this, new ProductoSeleccionadoEventArgs(producto));
+
                 this.DialogResult = DialogResult.OK;
                 this.Close();
             }
-        }
-
-        private void btnCancelar_Click(object sender, EventArgs e)
-        {
-            this.DialogResult = DialogResult.Cancel;
-            this.Close();
-        }
-
-        private void dataGridViewProductos_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
-        {
-            if (e.RowIndex >= 0)
+            else
             {
-                btnSeleccionar_Click(sender, e);
+                MessageBox.Show("Por favor, seleccione un producto", "Advertencia",
+                              MessageBoxButtons.OK, MessageBoxIcon.Warning);
             }
         }
+
+        public void ShowView()
+        {
+            this.Show();
+        }
+
+        DialogResult IProductoSearchView.ShowDialog()
+        {
+            return this.ShowDialog();
+        }
+
     }
 }
