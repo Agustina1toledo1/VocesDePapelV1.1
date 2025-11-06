4701a0a77f51f0a48473962c00ba74035bd5bc62
AgusT <agustina1toledo1@gmail.com>
2025-11-06 19:36:13 -0300

    Se creo el presenter de Vendedor COmprobante

---
+++ VocesDePapelV1.1/Views/IVendedorComprobanteView.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VocesDePapelV1._1.Views
+{
+    public interface IVendedorComprobanteView
+    {
+        //propiedades
+        string SearchNumeroComprobante { get; set; }  
+        string Message { get; set; }
+        bool IsSuccessful { get; set; }
+
+        //eventos
+        event EventHandler SearchComprobanteEvent;
+        event EventHandler ShowComprobanteEvent;
+
+        //metodos
+        void SetComprobanteFiltradoListBindingSource(object comprobanteList); //metodo para enlazar la lista de comprobantes
+        void Show(); //mostrar formulario
+    }
+}
--- VocesDePapelV1.1/Views/VendedorComprobanteView.cs
+++ VocesDePapelV1.1/Views/VendedorComprobanteView.cs
@@ -10,11 +10,38 @@ using System.Windows.Forms;
 
 namespace VocesDePapelV1._1.Views
 {
-    public partial class VendedorComprobanteView : Form
+    public partial class VendedorComprobanteView : Form, IVendedorComprobanteView
     {
+        private string message;
+        private bool isSuccessful;
         public VendedorComprobanteView()
         {
             InitializeComponent();
+            this.AutoScroll = true;
+            AssociateAndRaiseViewEvents();
+        }
+
+        private void AssociateAndRaiseViewEvents()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SearchNumeroComprobante {
+            get => throw new NotImplementedException(); set => throw new NotImplementedException();
+        }
+        public string Message { 
+            get => throw new NotImplementedException(); set => throw new NotImplementedException();
+        }
+        public bool IsSuccessful {
+            get => throw new NotImplementedException(); set => throw new NotImplementedException();
+        }
+
+        public event EventHandler SearchComprobanteEvent;
+        public event EventHandler ShowComprobanteEvent;
+
+        public void SetComprobanteFiltradoListBindingSource(object comprobanteList)
+        {
+            throw new NotImplementedException();
         }
     }
 }
